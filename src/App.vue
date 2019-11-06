2e5dd6c4ffbcc9f8c9b0ea1c31dff12301267e81
Alireza Hamid <alirezahamid20@gmail.com>
2019-11-06 10:02:33 +0330

    Add bottom navigation in footer App.vue and change icons

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,46 @@
 <template>
-  <v-app>
-    <v-app-bar app color="cyan" dark>
-      <div class="d-flex align-center">
-        <v-icon large class="mr-1">{{weather}}</v-icon>
-        <h1>Weather App</h1>
-      </div>
+  <div id="app">
+    <v-app>
+      <v-app-bar app color="cyan" dark>
+        <div class="d-flex align-center">
+          <v-icon large class="mr-1">mdi-weather-cloudy</v-icon>
+          <h1>Weather App</h1>
+        </div>
+        <v-spacer></v-spacer>
+        <v-btn href="https://gitlab.com/noghol/tutorials/weather" target="_blank" text>
+          <span class="mr-2 font-weight-bold">GitLab</span>
+          <v-icon>mdi-gitlab</v-icon>
+        </v-btn>
+      </v-app-bar>
 
-      <v-spacer></v-spacer>
+      <v-content>
+        <finder></finder>
+      </v-content>
 
-      <v-btn href="https://gitlab.com/noghol/tutorials/weather" target="_blank" text>
-        <span class="mr-2 font-weight-bold">GitLab</span>
-        <v-icon>{{gitlab}}</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <!-- Footer -->
+      <v-bottom-navigation color="white" background-color="cyan" dark>
+        <v-btn value="favorites">
+          <span>Finder</span>
+          <v-icon>mdi-cloud-search-outline</v-icon>
+        </v-btn>
 
-    <v-content></v-content>
-  </v-app>
+        <v-btn value="nearby">
+          <span>Weather</span>
+          <v-icon>mdi-weather-pouring</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import { mdiGitlab, mdiWeatherCloudy } from "@mdi/js";
+// import { mdiGitlab, mdiWeatherCloudy } from "@mdi/js";
+import Finder from "./components/Finder";
 export default {
   name: "App",
 
-  components: {},
+  components: { finder: Finder },
 
-  data: () => ({
-    gitlab: mdiGitlab,
-    weather: mdiWeatherCloudy
-  })
+  data: () => ({})
 };
 </script>

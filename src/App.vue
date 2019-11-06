e7fc4c539a2acd77dca35c83887ff2940fc832e7
Alireza Hamid <alirezahamid20@gmail.com>
2019-11-06 12:21:50 +0330

    install and config vue-router and reDesign footer

--- src/App.vue
+++ src/App.vue
@@ -14,33 +14,57 @@
       </v-app-bar>
 
       <v-content>
-        <finder></finder>
+        <router-view></router-view>
       </v-content>
 
-      <!-- Footer -->
+      <!-- Navigation -->
       <v-bottom-navigation color="white" background-color="cyan" dark>
-        <v-btn value="favorites">
+        <v-btn to="/" value="Finder">
           <span>Finder</span>
           <v-icon>mdi-cloud-search-outline</v-icon>
         </v-btn>
 
-        <v-btn value="nearby">
+        <v-btn to="/weather" value="Weather">
           <span>Weather</span>
           <v-icon>mdi-weather-pouring</v-icon>
         </v-btn>
       </v-bottom-navigation>
+
+      <!-- Footer -->
+      <v-footer dark padless>
+        <v-card class="flex" flat tile>
+          <v-card-text class="py-2 white--text text-center cyan lighten-2">
+            {{ new Date().getFullYear() }} —
+            <strong>Weather App By Alireza</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
     </v-app>
   </div>
 </template>
 
 <script>
 // import { mdiGitlab, mdiWeatherCloudy } from "@mdi/js";
-import Finder from "./components/Finder";
+// import Finder from "./components/Finder";
+// import Weather from "./components/Weather";
 export default {
   name: "App",
 
-  components: { finder: Finder },
-
-  data: () => ({})
+  data() {
+    return {
+      socialLinks: [
+        {
+          id: 1,
+          icon: "mdi-instagram",
+          href: "https://instagram.com/alirezahamidd"
+        },
+        {
+          id: 2,
+          icon: "mdi-gitlab",
+          href: "https://gitlab.com/noghol/tutorials/weather"
+        }
+      ]
+    };
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+Vue.config.productionTip = false
+
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import Routes from './services/routes/routes'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+})
 
-Vue.config.productionTip = false
+
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
